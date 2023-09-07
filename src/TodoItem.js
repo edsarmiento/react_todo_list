ba5a99879f5c8db8d0e4c8ae302ed1cfcd841da5
David Sarmiento <edst83@gmail.com>
2023-09-06 22:04:26 -0600

    fix css

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,9 +3,11 @@ import './TodoItem.css'
 function TodoItem(props){
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>V { props.completed }</span>
-      <p className={`TodoItem-p ${ props.completed && "TodoItem--complete"}`}>
-        { props.text }
+      <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>
+        V
+      </span>
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        {props.text}
       </p>
       <span className="Icon Icon-delete">X</span>
     </li>
